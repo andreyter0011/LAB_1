c27e862e813f4d4128c5b9d2752554c69058f87b
Andrey Tereschenko <worfdworfd@gmail.com>
2022-04-13 16:47:36 +0800

    первая лабораторная работа

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.invisibleLable1 = new Lab1.InvisibleLable();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // invisibleLable1
+            // 
+            this.invisibleLable1.AutoSize = true;
+            this.invisibleLable1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.invisibleLable1.Location = new System.Drawing.Point(295, 103);
+            this.invisibleLable1.Name = "invisibleLable1";
+            this.invisibleLable1.Size = new System.Drawing.Size(132, 22);
+            this.invisibleLable1.TabIndex = 0;
+            this.invisibleLable1.Text = "invisibleLable1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.invisibleLable1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private InvisibleLable invisibleLable1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ InvisibleLable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class InvisibleLable : Label
+    {
+        public InvisibleLable()
+        {
+            this.Font = new Font("Times New Roman", 14, FontStyle.Bold);
+            this.MouseEnter += new System.EventHandler(this.invisibleLable1_MouseEnter);
+        }
+        private void invisibleLable1_MouseEnter(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+    }
+}
